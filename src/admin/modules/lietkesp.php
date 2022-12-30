c7c7056e7ad0156c56aa393436ea08c3452d0771
tupiasplyths <20021409@vnu.edu.vn>
2022-12-30 18:10:43 +0700

    make products listing show all

--- src/admin/modules/lietkesp.php
+++ src/admin/modules/lietkesp.php
@@ -1,5 +1,6 @@
 <?php
-    $sql_lietkesp = "SELECT * FROM sanpham, danhmuc WHERE sanpham.id_danhmuc = danhmuc.id ORDER BY id_sanpham ASC";
+    $sql_lietkesp = "SELECT * FROM sanpham, danhmuc  ORDER BY id_sanpham ASC";
+    //WHERE sanpham.id_danhmuc = danhmuc.id
     $query_lietkesp = mysqli_query($mysqli, $sql_lietkesp);
 ?>
 
