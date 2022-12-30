16ec8ed2e9866a0d5ae10a51e1dba074ad508b43
tupiasplyths <20021409@vnu.edu.vn>
2022-12-30 19:55:32 +0700

    update database

--- src/admin/modules/lietkesp.php
+++ src/admin/modules/lietkesp.php
@@ -48,46 +48,4 @@
   ?>
 </table>
 
-<script>
-  const getCellValue = (tr, idx) =>
-    tr.children[idx].innerText || tr.children[idx].textContent;
-  
-
-  var comparer = function(idx, asc) { 
-      return function(a, b) {
-          return function(v1, v2) {
-              return (v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2)) 
-                  ? v1 - v2 : v1.toString().localeCompare(v2);
-          }(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
-      }
-  };
-  var count = 0;
-  document.querySelectorAll('th.sortable').forEach(th => th.addEventListener('click', (() => {                      
-      const table = th.closest('table');                
-      const tbody = table.querySelector('tbody');
-      Array.from(tbody.querySelectorAll('tr'))
-          .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-          .forEach(tr => tbody.appendChild(tr));              
-      
-      if (count%2!=0) {
-          editArrow();
-          th.classList.remove('asc');
-          th.classList.add('dsc');
-          
-      } else {
-          editArrow();
-          th.classList.remove('dsc');
-          th.classList.add('asc');
-      }
-      count++;
-      
-  })));
-
-  function editArrow() {
-      document.querySelectorAll('th.sortable').forEach(th => {
-          th.classList.remove('asc');
-          th.classList.remove('dsc');
-      });
-  }
-  
-</script>
+<script src="modules/sort.js"></script>
